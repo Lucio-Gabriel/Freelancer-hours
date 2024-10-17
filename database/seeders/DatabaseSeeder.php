bdb32bf814f5febbab4c5639ea7751c1e4c39482
Gabriel <suppershock777@gmail.com>
2024-10-17 10:39:03 -0300

    created our migrations and project factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,8 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::factory(200)->create();
+        User::factory()->count(200)->create();
 
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(fn (User $u) => Project::factory()->create(['created_by' => $u->id]));
 
     }
 }
